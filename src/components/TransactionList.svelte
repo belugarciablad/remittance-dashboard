3beb0b2ed3679745b66020c68477dbcc6fdf547f
Belen Garcia Blad <mbgarciablad@gmail.com>
2025-02-21 00:17:16 -0300

    install icon library and add copy to clipboard transaction id

--- src/components/TransactionList.svelte
+++ src/components/TransactionList.svelte
@@ -6,7 +6,8 @@
   import { getColorByStatus } from '../util/status-color-util';
   import TransactionDetail from './TransactionDetail.svelte';
   import SearchBar from './SearchBar.svelte';
-  import Filter from './Filter.svelte'
+  import Filter from './Filter.svelte';
+  import { Clipboard } from 'lucide-svelte';
 
   let isDetailModalOpen: boolean = false;
   let selectedTransaction: Transaction | null = null;
@@ -64,6 +65,12 @@
         dateRangeFilter = DateRangesEnum.All;
         selectedStatuses = [];
     }
+    
+
+    const copyToClipboard = (event: Event, text: string) => {
+        event.stopPropagation();
+        navigator.clipboard.writeText(text);
+    };
 
 </script>
 
@@ -106,7 +113,18 @@
 <tbody>
     {#each filteredTransactions as transaction (transaction.transaction_id)}
     <tr class="odd:bg-white even:bg-gray-50" on:click={() => openDetailModal(transaction)}>
-        <td class="px-4 py-2 border">{transaction.transaction_id}</td>
+        <td class="px-4 py-2 border">
+            <div class="flex items-center space-x-2">
+                <button 
+                on:click={(event) => copyToClipboard(event, transaction.transaction_id)} 
+                class="p-1 text-gray-500 hover:text-gray-700"
+                aria-label="Copy Transaction ID"
+                >
+                <Clipboard class="w-5 h-5" />
+            </button>
+            <span class="text-sm font-medium">{transaction.transaction_id}</span>
+            </div>
+        </td>
         <td class="px-4 py-2 border">{transaction.sender_whatsapp}</td>
         <td class="px-4 py-2 border">{transaction.receiver_whatsapp}</td>
         <td class="px-4 py-2 border">{transaction.amount_sent}</td>
